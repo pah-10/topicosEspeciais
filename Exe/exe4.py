827a06520049639afbb14544477c500f38d90d3d
Paola Capita <61804889+pah-10@users.noreply.github.com>
2022-08-24 20:07:55 -0300

    Update exe4.py

--- Exe/exe4.py
+++ Exe/exe4.py
@@ -4,11 +4,27 @@
 import os
 os.system('cls')
 
+
+#variaveis aux
+desconto = 0
+valorDescontado = 0
+
 #entrada dos dados
-nome = float(input("Informe o nome do produto: "))
-valor = float(input("Informe o valor da altura do triangulo: "))
+nome = input("Informe o nome do produto: ")
+valor = float(input("Informe o valor do produto: "))
 
-area = ((base * altura) / 2)
-
-#saida de dados
-print("A area do triangulo retangulo eh = {}".format(area))
+if (valor < 0 or valor < 50):
+	print("O valor inserido eh invalido! \nDigite APENAS produtos que tenham valores maiores que $50")
+else:
+	if (valor >= 50 and valor < 200):
+		desconto = 5
+	elif (valor >= 200 and valor < 500):
+		desconto = 6
+	elif (valor >= 500 and valor < 1000):
+		desconto = 7
+	elif (valor >= 1000):
+		desconto = 8
+		
+	valorDescontado = (valor - ((valor * desconto) / 100))
+	#saida de dados
+	print("\nO produto {0} com valor original de ${1} passa a valer ${2} com desconto de {3}%".format(nome, valor, round(valorDescontado,2), desconto))
