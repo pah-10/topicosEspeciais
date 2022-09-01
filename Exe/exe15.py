abc8debede3aebfc61e9930027b983a70523c05e
Paola Capita <61804889+pah-10@users.noreply.github.com>
2022-09-01 10:43:42 -0300

    Add files via upload

---
+++ Exe/exe15.py
@@ -0,0 +1,12 @@
+#Create a function checkPalindromethatgiven the string, check if it is a palindrome.
+# Example
+#   For inputString = "aabaa", the output should be checkPalindrome(inputString) = true;
+#   For inputString= "abac", the output should be checkPalindrome(inputString) = false;
+#   For inputString= "a", the output should be checkPalindrome(inputString) = true.
+
+def checkPalindrome(string):
+    print(string)
+
+palavra = input("Insira a palavra para verificação: ")
+
+checkPalindrome(palavra)
