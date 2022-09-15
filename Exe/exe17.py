1b6bb55d9e660a4459b139538bb2d85e0218cfd2
Paola Capita <61804889+pah-10@users.noreply.github.com>
2022-09-15 00:19:31 -0300

    Create exe17.py

---
+++ Exe/exe17.py
@@ -0,0 +1,21 @@
+# Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+# Example
+#  For inputArray= [3, 6, -2, -5, 7, 3], the output should be adjacentElementsProduct(inputArray) = 21.
+#  7 and 3 produce the largest product.
+
+import os
+import collections
+
+def adjacentElementsProduct(lista):
+	largest_product = lista[0] * lista[1]
+	
+	for i in range(len(lista)-1):
+		if(largest_product < (lista[i] * lista[i+1])):
+			largest_product = lista[i] * lista[i+1]
+	
+	print("O maior produto eh", largest_product)
+
+
+inputArray= [3, 6, -2, -5, 7, 3]
+
+adjacentElementsProduct(inputArray)
